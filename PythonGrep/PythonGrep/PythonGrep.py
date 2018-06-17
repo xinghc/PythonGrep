e01fb7b58c3638de877626d43c8cd9296ef44228
xinghc <xinghc@gmx.us>
2018-06-17 15:52:40 +0800

    Handling the rows and print only avalible data #2

--- PythonGrep/PythonGrep/PythonGrep.py
+++ PythonGrep/PythonGrep/PythonGrep.py
@@ -1,9 +1,20 @@
 import csv
 import urllib.request
 
+def RepresentsInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+
 url = 'http://www.twse.com.tw/fund/TWT38U?response=csv&date=20180605'
 response = urllib.request.urlopen(url)
-data = csv.reader(response.read().decode('Big5','replace'))
+data = csv.reader(response.read().decode('Big5','replace').splitlines())
+next(data)
 
 for row in data:
-    print(', '.join(row))
+    if RepresentsInt(row[1]): # Skip those we are not able recognized
+        # ToDo: Handle those special code name (e.g., ="00637L")
+        print(', '.join(row))
+
